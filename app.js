e3982b0dc228664f1046ee2bd5f77a2d4f1eb7be
zhangyanfa <yanfaz@cn.ibm.com>
2020-08-26 06:40:10 +0800

    test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ module.exports = (/*options*/) => {
   app.get('/', (req, res) => {
     // Use req.log (a `pino` instance) to log JSON:
     req.log.info({message: 'Hello from Appsody!'});
-    res.send('Hello from Appsody! My first Nodejs DevOps app!!!');
+    res.send('Hello from Appsody! My first Nodejs DevOps app!!!!');
   });
 
   return app;
